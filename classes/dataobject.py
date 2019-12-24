8778f4cd059df40fd8a20394496b0b9d8fa19c6d
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2019-12-24 15:32:22 -0500

    Implement ABC to dataobject

--- classes/dataobject.py
+++ classes/dataobject.py
@@ -1,23 +1,52 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-class DataObject:
+class ChannelError(Exception):
+	def __init__(self, c, channels, message):
+		self.c = c
+		self.channels = channels
+		self.message = message
+		
+class ChannelNotImplementedError(Exception):
+	def __init__(self, message):
+		self.message = message
+
+		
+class DataObject(ABC):
+	@classmethod
 	def __init__(self, filepath, name=""):
 		self.filepath = filepath
 		self.name = name
 		self.data = np.zeros(0)
 		self.resolution = -1
 		self.length = -1
+		self.scores = np.zeros(0)
+		self.times = np.zeros(0)
+		self.scoreLength = -1
 		
+	@classmethod
 	def __str__(self):
 		return type(self).__name__ + ": " + self.name + " at " + self.filepath
 		
-	def read(self):
+	@abstractmethod
+	def createName(self):
+		raise NotImplementedError( "No createName function implemented" )
+		
+	@abstractmethod
+	def read(self, args*, kwargs*):
 		raise NotImplementedError( "No read function implemented" )
 		
+	@classmethod
 	def get(self, i, k=None):
+		if self.data.shape[0] == 0:
+			return False
 		if isinstance(k, int):
 			return self.data[i:k]
 		else:
 			return self.data[i]
 		
+	@classmethod
+	def zip(self, arr):
+		indices = np.arange(arr.size)
+		return np.stack((indices,arr), axis=1)
 	
--- classes/smrEMG.py
+++ classes/smrEMG.py
@@ -4,29 +4,17 @@ import numpy as np
 
 from parameters import *
 
-class ChannelError(Exception):
-	def __init__(self, c, channels, message):
-		self.c = c
-		self.channels = channels
-		self.message = message
-		
-class ChannelNotImplementedError(Exception):
-	def __init__(self, message):
-		self.message = message
-
 
 class smrEMG(DataObject):
 	def __init__(self, filepath, name=""):
 		super().__init__(filepath, name=name)
-		self.channel = ""
-		self.scoreChannel = ""
-		self.scores = np.zeros(0)
-		self.times = np.zeros(0)
-		self.first_samples
-		self.min_interval
 		
-	def read(self):
-		if self.channel == "" or self.scoreChannel == "":
+	def createName(self):
+		self.name = self.filepath.split('/')[-1].split('.mat')[0]
+		return self.name
+		
+	def read(self, c="", s=""):
+		if c == "" or s == "":
 			raise ChannelNotImplementedError("EMG channel or score channel has not been specified")
 		try:
 			matfile = loadmat(filepath)
@@ -34,30 +22,21 @@ class smrEMG(DataObject):
 			raise FileNotFoundError("No such file or directory: " + filepath)
 		for field in matfile.keys():
 			if '_Ch' in field:
-				if self.channel == matfile[field][0][0][0][0]:
-					self.data = (matfile[field][0][0][8]).flatten()
+				if c == matfile[field][0][0][0][0]:
+					self.data = self.zip((matfile[field][0][0][8]).flatten())
 					self.resolution = matfile[field][0][0][2]
 					self.length = matfile[field][0][0][7]
-				elif self.scoreChannel == matfile[field][0][0][0][0]:
+				elif s == matfile[field][0][0][0][0]:
 					self.scores = matfile[field][0][0][7]
 					self.times = matfile[field][0][0][5]
+					self.scoreLength = len(self.scores)
 		if self.data.size == 0 or self.scores.size == 0 or self.times.size == 0:
 			channels = []
 			for field in matfile.keys():
 				channels.append(matfile[field][0][0][0][0])
 			if self.data.size == 0:
-				raise ChannelError(self.channel, channels, "EMG channel not found")
+				raise ChannelError(c, channels, "EMG channel not found")
 			else:
-				raise ChannelError(self.scoreChannel, channels, "Score channel not found")
+				raise ChannelError(s, channels, "Score channel not found")
 		self.data = np.absolute(self.data)
-		try:
-			self.first_samples = FIRST_TIME / (self.resolution * 1000)
-			self.min_interval = MIN_INTERVAL_TIME / (self.resolution * 1000)
-		except:
-			raise RuntimeError("Something went wrong setting up parameters for data")
 		
-	def selectChannel(self, channel):
-		self.channel = str(channel)
-		
-	def selectScoreChannel(self, channel):
-		self.scoreChannel = str(channel)
